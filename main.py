5f1ad19b382df7156aaaf92566f1dbc94b33be46
Sami Elkhayri <selkhayri@gmail.com>
2025-01-30 11:06:54 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 11:04:04 2025
+
+@author: sami
+"""
+
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
